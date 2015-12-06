5d0475ac689b61a9e58371355b03b00e39d78ac7
trizen <trizenx@gmail.com>
2015-12-06 18:23:17 +0200

    Added `pypy` to the list of interpreters. new file:   Interpreted/coin-sums/csum.lua new file:   Interpreted/coin-sums/csum.php

---
+++ Interpreted/coin-sums/csum.php
@@ -0,0 +1,21 @@
+<?php
+function num_paths($startsum, $lastcoin) {
+
+    static $coins = array(1, 2, 5, 10, 20, 50, 100, 200);
+
+    if ($startsum == 250) {
+        return 1;
+    }
+
+    $paths = 0;
+    foreach ($coins as $coin) {
+        if (($lastcoin >= $coin) && ($startsum <= (250 - $coin))) {
+            $paths += num_paths($startsum + $coin, $coin);
+        }
+    }
+
+    return $paths;
+}
+
+echo num_paths(0, 250), "\n";
+?>
